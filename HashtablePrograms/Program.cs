d8e445d96df93a107618a5c75d3733ce54cafbb2
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-02-06 23:24:24 +0530

    Adding program to remove word from phrase

---
+++ HashtablePrograms/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashtablePrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Hashtable programs");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(20);
+            Console.WriteLine(hash.IsEmpty());
+            hash.Add("0", "paranoids");
+            hash.Add("1", "are");
+            hash.Add("2", "not");
+            hash.Add("3", "paranoid");
+            hash.Add("4", "because");
+            hash.Add("5", "they");
+            hash.Add("6", "are");
+            hash.Add("7", "paranoid");
+            hash.Add("8", "but");
+            hash.Add("9", "they");
+            hash.Add("10", "keep");
+            hash.Add("11", "putting");
+            hash.Add("12", "themselves");
+            hash.Add("13", "deliberately");
+            hash.Add("14", "into");
+            hash.Add("15", "paranoid");
+            hash.Add("16", "avoidable");
+            hash.Add("17", "situations");
+            Console.WriteLine(hash.GetV("5").GetHashCode());
+            Console.WriteLine(hash.GetV("0").GetHashCode());
+            Console.WriteLine($"size is: {hash.Getsize()}");
+            hash.Remove("16");
+            Console.WriteLine($"size is: {hash.Getsize()}");
+            Console.ReadLine();
+        }
+
+    }
+    
+}
