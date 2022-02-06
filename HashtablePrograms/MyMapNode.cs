69e74252d3c693529c4b7da6b863ba9b498770ef
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-02-06 23:13:50 +0530

    Adding program to find frequency of word

--- HashtablePrograms/MyMapNode.cs
+++ HashtablePrograms/MyMapNode.cs
@@ -6,76 +6,86 @@ using System.Threading.Tasks;
 
 namespace HashtablePrograms
 {
-        class MyMapNode<K, V>
+    class MyMapNode<K, V>
+    {
+        private int size;
+        private LinkedList<KeyValue<K, V>>[] items;
+
+        public MyMapNode(int size)
         {
-            private int size;
-            private LinkedList<KeyValue<K, V>>[] items;
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        protected int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int pos)
+        {
+            LinkedList<KeyValue<K, V>> linkedlist = items[pos];
+            if (linkedlist == null)
+            {
+                linkedlist = new LinkedList<KeyValue<K, V>>();
+                items[pos] = linkedlist;
+            }
+            return linkedlist;
+        }
 
-            public MyMapNode(int size)
+        public V GetV(K key)
+        {
+            int pos = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(pos);
+            foreach (KeyValue<K, V> item in linkedlist)
             {
-                this.size = size;
-                this.items = new LinkedList<KeyValue<K, V>>[size];
-            }
-            protected int GetArrayPosition(K key)
-            {
-                int position = key.GetHashCode() % size;
-                return Math.Abs(position);
-            }
-            protected LinkedList<KeyValue<K, V>> GetLinkedList(int pos)
-            {
-                LinkedList<KeyValue<K, V>> linkedlist = items[pos];
-                if (linkedlist == null)
+                if (item.Key.Equals(key))
                 {
-                    linkedlist = new LinkedList<KeyValue<K, V>>();
-                    items[pos] = linkedlist;
+                    return item.Value;
                 }
-                return linkedlist;
             }
-
-            public V GetV(K key)
+            return default(V);
+        }
+        public void Add(K key, V value)
+        {
+            int pos = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(pos);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedlist.AddLast(item);
+        }
+        public void Remove(K key)
+        {
+            int pos = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(pos);
+            bool isFound = false;
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in linkedlist)
             {
-                int pos = GetArrayPosition(key);
-                LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(pos);
-                foreach (KeyValue<K, V> item in linkedlist)
+                if (item.Key.Equals(key))
                 {
-                    if (item.Key.Equals(key))
-                    {
-                        return item.Value;
-                    }
+                    isFound = true;
+                    foundItem = item;
                 }
-                return default(V);
-            }
-            public void Add(K key, V value)
-            {
-                int pos = GetArrayPosition(key);
-                LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(pos);
-                KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
-                linkedlist.AddLast(item);
-            }
-            public void Remove(K key)
-            {
-                int pos = GetArrayPosition(key);
-                LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(pos);
-                bool isFound = false;
-                KeyValue<K, V> foundItem = default(KeyValue<K, V>);
-                foreach (KeyValue<K, V> item in linkedlist)
+                if (isFound == true)
                 {
-                    if (item.Key.Equals(key))
-                    {
-                        isFound = true;
-                        foundItem = item;
-                    }
-                    if (isFound == true)
-                    {
-                        linkedlist.Remove(foundItem);
-                    }
+                    linkedlist.Remove(foundItem);
                 }
             }
         }
-        public struct KeyValue<k, v>
+        public bool IsEmpty()
         {
-            public k Key { get; set; }
-            public v Value { get; set; }
+            if (Getsize() <= 0)
+                return true;
+            else
+                return false;
         }
-    
+        public int Getsize()
+        {
+            return size;
+        }
+    }
+    public struct KeyValue<k, v>
+    {
+        public k Key { get; set; }
+        public v Value { get; set; }
+    }
 }
